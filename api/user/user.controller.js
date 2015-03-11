6020468276f53eb69cce2347d5777425c9493851
Peter Anderson <anderson.peter@byu.edu>
2015-03-10 23:34:03 -0600

    Added User model

---
+++ api/user/user.controller.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var UserController = {
+    login : function(req, res, next) {
+
+    },
+
+    register : function(req, res, next) {
+
+    }
+}
+
+module.exports = UserController;
