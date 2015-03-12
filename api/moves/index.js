921f11b5b8483111c3905d0802b519950cb475ea
Mikey Murphy <mikeyamadeo@gmail.com>
2015-03-11 18:46:52 -0600

    app skeleton. api/moves is the example

--- api/moves/index.js
+++ api/moves/index.js
@@ -2,11 +2,27 @@
 
 var express = require('express');
 
-var controller = require('./moves.controller');
+var ctrl = require('./moves.ctrl');
 
 var router = express.Router();
 
-router.post('/Road_Building', controller.roadBuilding);
+router.post('/sendChat',        ctrl.sendChat);
+router.post('/rollNumber',      ctrl.rollNumber);
+router.post('/robPlayer',       ctrl.robPlayer);
+router.post('/finishTurn',      ctrl.finishTurn);
+router.post('/buyDevCard',      ctrl.buyDevCard);
+router.post('/Year_of_Plenty',  ctrl.yearOfPlenty);
+router.post('/Road_Building',   ctrl.roadBuilding);
+router.post('/Soldier',         ctrl.Soldier);
+router.post('/Monopoly',        ctrl.Monopoly);
+router.post('/Monument',        ctrl.Monument);
+router.post('/buildRoad',       ctrl.buildRoad);
+router.post('/buildSettlement', ctrl.buildSettlement);
+router.post('/buildCity',       ctrl.buildCity);
+router.post('/offerTrade',      ctrl.offerTrade);
+router.post('/acceptTrade',     ctrl.acceptTrade);
+router.post('/maritimeTrade',   ctrl.maritimeTrade);
+router.post('/discardCards',    ctrl.discardCards);
 
 
 module.exports = router;
--- api/moves/moves.controller.js
+++ api/moves/moves.controller.js
@@ -5,6 +5,7 @@ var _ = require('lodash');
 /**
  * Example of getting access to required models:
  *
+ * var Model = require('../model');
  * var MovesModel = require('./moves.model');
  */
 
@@ -12,10 +13,45 @@ var _ = require('lodash');
 var MovesController = {
   /** CREATE **/
 
-  roadBuilding: function(req, res, next) {
-    //logic and stuff.
-    //response and stuff
-  }
+  /**
+   * @desc store a chat message & return a response
+   * @arg {object} req - http request object
+   * @arg {object} res - http response object
+   * @arg {function} next - next command
+   */
+  sendChat: function(req, res, next) {
+    /* Psuedo code
+        //Example of how we can store commands for the command pattern
+        var body = req.body;
+        Model.storeCommand(body);
+
+        //store chat message
+        MovesModel.storeMessage(body, function(err, msg) {
+            //send back response after message stored
+            if(err) return next(err);
+            res.status(201);
+        })
+    */
+  },
+
+  rollNumber: function(req, res, next) {},
+  robPlayer: function(req, res, next) {},
+  finishTurn: function(req, res, next) {},
+  buyDevCard: function(req, res, next) {},
+  yearOfPlenty: function(req, res, next) {},
+  roadBuilding: function(req, res, next) {},
+  Soldier: function(req, res, next) {},
+  Monopoly: function(req, res, next) {},
+  Monument: function(req, res, next) {},
+  buildRoad: function(req, res, next) {},
+  buildSettlement: function(req, res, next) {},
+  buildCity: function(req, res, next) {},
+  offerTrade: function(req, res, next) {},
+  acceptTrade: function(req, res, next) {},
+  maritimeTrade: function(req, res, next) {},
+  discardCards: function(req, res, next) {},
+  roadBuilding: function(req, res, next) {},
+
 };
 
 module.exports = MovesController;
