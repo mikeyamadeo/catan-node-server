28e3fcd96191b62cb00b12b0a1763386d310a9cf
x3ro <atsoderquist@gmail.com>
2015-04-01 21:18:42 -0600

    added logging

--- api/moves/moves.controller.js
+++ api/moves/moves.controller.js
@@ -64,6 +64,9 @@ var MovesController = {
       var map = model.game.map;
       var bank = model.game.bank;
       var numberRolled = req.body.number;
+      var currPlayerIndex = players[model.game.turnTracker.currentTurn]
+
+      var logMessage = players[currPlayerIndex].name + " rolled a "  + numberRolled;
         var resourceChanges = [
             {
                 player : 0,
@@ -81,7 +84,7 @@ var MovesController = {
                 player : 3,
                 resourceMap : {}
             }
-        ];
+        ];      
 
       if (numberRolled == 7) {
         var discardHuh = false;
@@ -105,6 +108,11 @@ var MovesController = {
             MovesModel.rollNumber(gameId, "Robbing", [], function(err, game) {
               if(!req.command) {
                 command.addCommand(gameId, body);
+                model.game.log.push({
+                  message:logMessage,
+                  source:players[currPlayerIndex].name
+                });
+                model.save();
                 return res.status(200).json(game.game);
               }
             });
@@ -165,6 +173,11 @@ var MovesController = {
 
             MovesModel.rollNumber(gameId, "Playing", resourceChanges, function(err, game) {
                 if(!req.command) {
+                  model.game.log.push({
+                    message:logMessage,
+                    source:players[currPlayerIndex].name
+                  });
+                  model.save();
                   command.addCommand(gameId, body);
                   return res.status(200).json(game.game);
                 }
@@ -260,6 +273,14 @@ var MovesController = {
                   return res.status(500).send("Server Error");
               } else {
                 if(!req.command) {
+                  var players = result.game.players;
+                  var currPlayerIndex = result.game.turnTracker.currentTurn;
+                  var logMessage = players[currPlayerIndex].name + " moved the robber";
+                  result.game.log.push({
+                    message:logMessage,
+                    source:players[currPlayerIndex].name
+                  });
+                  result.save();
                   command.addCommand(req.game, req.body); 
                   return res.status(200).json(result.game);
                 }
@@ -296,6 +317,14 @@ var MovesController = {
             return res.status(500).send(err)
         }
         if(!req.command) {
+          var players = game.game.players;
+          var currPlayerIndex = game.game.turnTracker.currentTurn;
+          var logMessage = players[currPlayerIndex].name + " finished their turn";
+          game.game.log.push({
+            message:logMessage,
+            source:players[currPlayerIndex].name
+          });
+          game.save();
           command.addCommand(req.game, req.body);
           res.json(game.game);
         }
@@ -381,6 +410,14 @@ var MovesController = {
         var random = Math.floor(Math.random() * allCards.length);
         MovesModel.buyDevCard(gameId, playerId, allCards[random], function(err, result) {
         if(!req.command) {
+          var players = game.game.players;
+          var currPlayerIndex = game.game.turnTracker.currentTurn;
+          var logMessage = players[currPlayerIndex].name + " bought a dev card";
+          game.game.log.push({
+            message:logMessage,
+            source:players[currPlayerIndex].name
+          });
+          game.save();
           command.addCommand(req.game, req.body); 
           return res.status(200).json(result.game);
         }
@@ -840,6 +877,15 @@ var MovesController = {
                 if (err) {
                       return callback(err); 
                 }
+
+                var players = game.game.players;
+                var currPlayerIndex = game.game.turnTracker.currentTurn;
+                var logMessage = players[currPlayerIndex].name + " built a road";
+                game.game.log.push({
+                  message:logMessage,
+                  source:players[currPlayerIndex].name
+                });
+                game.save();
                 console.log("road built");
                 return callback(null, game);
             });
@@ -913,6 +959,15 @@ var MovesController = {
                 if (err) {
                       return callback(err); 
                 }
+
+                var players = game.game.players;
+                var currPlayerIndex = game.game.turnTracker.currentTurn;
+                var logMessage = players[currPlayerIndex].name + " built a settlement";
+                game.game.log.push({
+                  message:logMessage,
+                  source:players[currPlayerIndex].name
+                });
+                game.save();
                 console.log("settlement built");
                 return callback(null, game);
             });
@@ -986,6 +1041,15 @@ var MovesController = {
                 if (err) {
                       return callback(err); 
                 }
+
+                var players = game.game.players;
+                var currPlayerIndex = game.game.turnTracker.currentTurn;
+                var logMessage = players[currPlayerIndex].name + " built a city";
+                game.game.log.push({
+                  message:logMessage,
+                  source:players[currPlayerIndex].name
+                });
+                game.save();
                 console.log("city built");
                 return callback(null, game);
             });
